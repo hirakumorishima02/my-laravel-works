482733f4dc97fd93298df422d25c3d62d6d5fd55
EC2 Default User <ec2-user@ip-172-31-28-88.ap-southeast-1.compute.internal>
2019-08-12 02:02:06 +0000

    Add Layout image

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,10 +1,12 @@
 <template>
-    <nav class='nav' style='margin: 30px 0 30px 0;'>
+  <div>
+    <nav class='nav' style='margin: 30px 0 30px 0;padding-bottom:200px;padding-top:50px;background:#212226;'>
       <router-link to="/" style='color:#989faa;padding-right:0;' exact><i class="fab fa-fort-awesome"></i></router-link>
       <span v-for='{id, name} in list' :key='id' class='nav-span'>
         <router-link :to="`/product/${ id }`" style='color:#989faa;'>{{ name }}</router-link>
       </span>
     </nav>
+  </div>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,39 @@
 <template>
-    <div class="home">
-        <h1 class='title'>My Laravel Works</h1>
-            <a href='https://github.com/hirakumorishima02' target="_blank"><i class="fab fa-github-square fa-5x"></i></a>
-            <a href='https://twitter.com/hiwriterprogram' target="_blank"><i class="fab fa-twitter-square fa-5x"></i></a>
-            <a href='https://www.facebook.com/ujinchu' target="_blank"><i class="fab fa-facebook-square fa-5x"></i></a>
+    <div>
+        <div class="home">
+            <h1 class='title'>My Laravel Works</h1>
+                <a href='https://github.com/hirakumorishima02' target="_blank"><i class="fab fa-github-square fa-5x"></i></a>
+                <a href='https://twitter.com/hiwriterprogram' target="_blank"><i class="fab fa-twitter-square fa-5x"></i></a>
+                <a href='https://www.facebook.com/ujinchu' target="_blank"><i class="fab fa-facebook-square fa-5x"></i></a>
+        </div>
+        <Footer />
     </div>
 </template>
+
+
+<script>
+import products from '@/api/products.js'
+import Footer from '@/views/Footer.vue'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+      Footer
+  },
+    props: { id: Number },
+    data() {
+        return {
+            item : null,
+        }
+    },
+    computed: mapGetters('product', ['detail']),
+    watch: { 
+        id: {
+            handler() {
+                products.asyncFind(this.id, item => {
+                    this.item = item
+                })
+            }, immediate: true
+        }    
+    }
+}
+</script> 
