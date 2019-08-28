c9e15842c6cb9b041c9fee931d73b470962f04cc
EC2 Default User <business.ujinchu@gmail.com>
2019-08-28 08:35:18 +0000

    Add Ecommerce

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
                 <a href='https://github.com/hirakumorishima02' target="_blank"><i class="fab fa-github-square fa-5x"></i></a>
                 <a href='https://twitter.com/hiwriterprogram' target="_blank"><i class="fab fa-twitter-square fa-5x"></i></a>
                 <a href='https://www.facebook.com/ujinchu' target="_blank"><i class="fab fa-facebook-square fa-5x"></i></a>
+                <a href='https://www.facebook.com/ujinchu' target="_blank"><i class="fab fa-blogger fa-5x"></i></a>
         </div>
         <Footer />
     </div>
